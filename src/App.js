7510488e5d1f5d9b2b466ab3db2e638adb8dd841
LJEsp <esparragoleandroj@gmail.com>
2019-01-02 22:21:56 +0800

    Edi wow

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Spring, Transition, config } from "react-spring";
+
+const StyledApp = styled.div``;
+
+const StyledBox = styled.div`
+  box-sizing: border-box;
+  width: 5em;
+  height: 5em;
+  background-color: orange;
+  border-radius: 100%;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <StyledApp>
+        <h2>Spring</h2>
+
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Default</h3>
+        <Spring
+          config={config.default}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Gentle</h3>
+        <Spring
+          config={config.gentle}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Wobbly</h3>
+        <Spring
+          config={config.wobbly}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Stiff</h3>
+        <Spring
+          config={config.stiff}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Slow</h3>
+        <Spring
+          config={config.slow}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Molasses</h3>
+        <Spring
+          config={config.molasses}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+
+        <h3>Delay</h3>
+        <Spring
+          config={{ delay: 2000 }}
+          from={{ transform: "scale(0)" }}
+          to={{ transform: "scale(1)" }}
+        >
+          {props => <StyledBox style={props} />}
+        </Spring>
+      </StyledApp>
     );
   }
 }
